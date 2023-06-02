135081ea9c726e6b0e089164b36edc1bc220ed71
kdb0074 <110100310+kdb0074@users.noreply.github.com>
2023-06-02 17:44:26 +0530

    sc

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,21 +1,22 @@
-import myImg1 from '../Assets/Screenshot (6).png'
-import myImg2 from '../Assets/Screenshot (2).png'
-import myImg3 from '../Assets/Screenshot (4).png'
+import myImgA from '../Assets/ecom.png'
+import myImgB from '../Assets/cry.png'
+import myImgC from '../Assets/imagetext.png'
+
 
 export const data =[
     { id:1,
         name:"Ecommerce",
-        image: myImg1,
+        image: myImgA,
         github:"https://github.com/kdb0074/Eccomerce",
         live:"https://elaborate-hummingbird-7eb2fe.netlify.app/",},
-    { id:1,
+    { id:2,
         name:"Crypto App",
-        image: myImg2,
+        image: myImgB,
         github:"https://github.com/kdb0074/Crypto-Info",
         live:"https://zesty-otter-aecee5.netlify.app/",},
-    { id:1,
+    { id:3,
         name:"TextEditor",
-        image: myImg3,
+        image: myImgC,
         github:"https://github.com/kdb0074/TextEditor",
         live:"https://euphonious-palmier-d28073.netlify.app/",},
     
